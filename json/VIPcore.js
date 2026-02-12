2d7141b8968c4a5807a16ba346512d878a62de2c
dieuduongna <113266844+luongnguyenthanh@users.noreply.github.com>
2026-02-12 15:36:00 +0700

    Update VIPcore.js

--- json/VIPcore.js
+++ json/VIPcore.js
@@ -14,13 +14,12 @@ $(function() {
     // 2. Gán ngày mặc định là hôm nay
     $("#evalDate").datepicker("setDate", new Date());
 
-    // 3. Cho phép hiện lịch khi click vào ô input (dù có readonly)
+    // 3. Cho phép hiện lịch khi click vào ô input
     $("#evalDate").on("click", function() {
         $(this).datepicker("show");
     });
-});
 
-    // Chọn mức VIP
+    // 4. Chọn mức VIP 
     $(".vip-option").on("click", function() {
         $(".vip-option").removeClass("selected");
         $(this).addClass("selected");
@@ -29,12 +28,12 @@ $(function() {
         $("#result-score").text(score);
         
         let advice = "";
-        if (score === 0) advice = "Không có dấu hiệu viêm TM→ Theo dõi, đánh giá ít nhất 2 lần/ngày.";
-        else if (score === 1) advice = "Có thể là giai đoạn sớm của viêm TM → Tăng cường theo dõi, đánh giá (ít nhất mỗi 6 giờ/lần.";
-        else if (score === 2) advice = "Giai đoạn sớm của viêm TM → Thay đường truyền, đặt tại vị trí khác. Ghi chép HSBA. Cân nhắc điều trị";
+        if (score === 0) advice = "Không có dấu hiệu viêm TM → Theo dõi, đánh giá ít nhất 2 lần/ngày.";
+        else if (score === 1) advice = "Có thể là giai đoạn sớm của viêm TM → Tăng cường theo dõi, đánh giá ít nhất mỗi 6 giờ/lần.";
+        else if (score === 2) advice = "Giai đoạn sớm của viêm TM → Thay đường truyền, đặt tại vị trí khác. Ghi chép HSBA. Cân nhắc điều trị.";
         else if (score === 3) advice = "Giai đoạn trung bình của viêm TM → Thay đường truyền, đặt tại vị trí khác. Ghi chép HSBA. Điều trị.";
-        else if (score === 4 ) advice = "Giai đoạn tiến triển của viêm TM hoặc bắt đầu huyết khối TM → Thay đường truyền. Cân nhắc điều trị kháng sinh, cấy máu, cấy dịch chân kim. Ghi chép HSBA. Điều trị";
-        else if (score === 5) advice = "Giai đoạn tiến triển của huyết khối → Thay đường truyền. Cân nhắc điều trị kháng sinh, cấy máu, cấy dịch chân kim. Ghi chép HSBA. Điều trị";
+        else if (score === 4) advice = "Giai đoạn tiến triển của viêm TM hoặc bắt đầu huyết khối TM → Thay đường truyền. Cân nhắc điều trị kháng sinh, cấy máu, cấy dịch chân kim. Ghi chép HSBA. Điều trị.";
+        else if (score === 5) advice = "Giai đoạn tiến triển của huyết khối → Thay đường truyền. Cân nhắc điều trị kháng sinh, cấy máu, cấy dịch chân kim. Ghi chép HSBA. Điều trị.";
         
         $("#result-advice").text(advice);
     });
