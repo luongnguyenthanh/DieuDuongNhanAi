112a3c34e6e2a6030514dbba4b4906ec91dc902a
dieuduongna <113266844+luongnguyenthanh@users.noreply.github.com>
2026-02-12 15:41:19 +0700

    Fix comment formatting in VIPcore.js

--- json/VIPcore.js
+++ json/VIPcore.js
@@ -5,7 +5,7 @@ $(function() {
         changeMonth: true,
         changeYear: true,
         yearRange: "2025:2050",
-        maxDate: 0, // 0 nghĩa là hôm nay, chặn tuyệt đối ngày tương lai
+        maxDate: 0, 
         showButtonPanel: true,
         dayNamesMin: ["CN","T2","T3","T4","T5","T6","T7"],
         monthNamesShort: ["Th1","Th2","Th3","Th4","Th5","Th6","Th7","Th8","Th9","Th10","Th11","Th12"]
